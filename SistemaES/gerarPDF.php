1ecb61727f53b4e39299b2c5068f9a19819242a9
Nathany Salles <nathany.salles@gmail.com>
2020-09-30 21:59:58 -0300

    gerar PDF
    gerar PDF

--- SistemaES/gerarPDF.php
+++ SistemaES/gerarPDF.php
@@ -1,4 +1,8 @@
 <?php
+require_once '../assets/php/classes/classRelatorio.php';
+$relat = new Relatorio();
+$relat->setunidadeAtendimento($_POST['unidadeAtendimento']);
+$stmt = $relat->viewUnidade();
 
 include 'pdf-php/src/Cezpdf.php'; // Or use 'vendor/autoload.php' when installed through composer
 
@@ -15,8 +19,14 @@ $size = 12;
 // Modified to use the local file if it can
 $pdf->openHere('Fit');
 
+while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+
 // Output some colored text by using text directives and justify it to the right of the document
-$pdf->ezText("PDF with some <c:color:1,0,0>blue</c:color> <c:color:0,1,0>red</c:color> and <c:color:0,0,1>green</c:color> colours", $size, ['justification' => 'right']);
+	//$pdf->ezText("PDF with some <c:color:1,0,0>blue</c:color> <c:color:0,1,0>red</c:color> and <c:color:0,0,1>green</c:color> colours", $size, ['justification' => 'right']);
+	$pdf->ezText($row->unidadeAtendimento);
+}
+;
+
 // Output the pdf as stream, but uncompress
 $pdf->ezStream(['compress' => 0]);
 ?>
